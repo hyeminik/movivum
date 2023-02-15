dccb71d53250e43cce63c57219d23f264168cb14
p-jina <p.jinapark@gmail.com>
2023-02-15 17:47:57 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 $(function(){
-    // 인기순
-$.ajax({
-    url:"https://api.themoviedb.org/3/movie/popular?api_key=ce8ce557f85eb5b4175ee66f08002107&page=1&language=ko-KR",
-    data:{"key":"value"},
-    type:"post",
-    success:function(json){
-        console.log(json);
-        $("#popular_wrap").html('')
-        let movie_list=json.results;
 
-    for(let i=0;i<5;i++){
-        let page=`
-        <div class="movie_wrap">
-            <div class="movie_poster">
-                <img src="${'https://image.tmdb.org/t/p/w342/'+movie_list[i].poster_path}">
-            </div>
-            <div class="movie_info">
-                <span class="movie_title">${movie_list[i].title}</span>
-                <span class="movie_review">
-                    <span class="material-symbols-outlined star">grade</span>${movie_list[i].vote_average}
-                    &nbsp;|&nbsp;리뷰(999)
-                </span>
-            </div>
-        </div>`
-            $("#popular_wrap").append(page);
+    $('[data-vbg]').youtube_background();
+    // 인기순
+    $.ajax({
+        url:"https://api.themoviedb.org/3/movie/popular?api_key=ce8ce557f85eb5b4175ee66f08002107&page=1&language=ko-KR",
+        data:{"key":"value"},
+        type:"post",
+        success:function(json){
+            console.log(json);
+            $("#popular_wrap").html('')
+            let movie_list=json.results;
+
+        for(let i=0;i<10;i++){
+            let page=`
+            <div class="swiper-slide">
+                <div class="movie_poster">
+                    <img src="${'https://image.tmdb.org/t/p/w342/'+movie_list[i].poster_path}">
+                </div>
+                <div class="movie_info">
+                    <span class="movie_title">${movie_list[i].title}</span>
+                    <span class="movie_review">
+                        <span class="material-symbols-outlined star">grade</span>${movie_list[i].vote_average}
+                        &nbsp;|&nbsp;리뷰(999)
+                    </span>
+                </div>
+            </div>`
+                $("#popular_wrap").append(page);
+            }
         }
-      }
     })
     // 현재상영순
     $.ajax({
@@ -36,9 +38,9 @@ $.ajax({
             console.log(json);
             $("#now_playing_wrap").html('')
             let movie_list=json.results;
-        for(let i=0;i<5;i++){
+        for(let i=0;i<10;i++){
             let page=`
-            <div class="movie_wrap">
+            <div class="swiper-slide">
                 <div class="movie_poster">
                     <img src="${'https://image.tmdb.org/t/p/w342/'+movie_list[i].poster_path}">
                 </div>
@@ -65,9 +67,9 @@ $.ajax({
             $("#recommend_wrap").html('');
             let movie_list=json.results;
             console.log(movie_list);
-        for(let i=0;i<5;i++){
+        for(let i=0;i<10;i++){
             let page=`
-            <div class="movie_wrap">
+            <div class="swiper-slide">
                 <div class="movie_poster">
                     <img src="${'https://image.tmdb.org/t/p/w342/'+movie_list[i].poster_path}">
                 </div>
@@ -85,11 +87,21 @@ $.ajax({
     })
     // 로그인 아이콘 선택 시 모달창 d/p
     $('.login_icon').click(function(){
-        $('.bg').fadeIn();
+        $('.blackbg').fadeIn();
         $('.login_box').show();
     })
-    $('.bg').click(function(){
-        $('.bg').fadeOut();
+    $('.blackbg,login_closebtn').click(function(){
+        $('.blackbg').fadeOut();
         $('.login_box').hide();
     })
+
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        centeredSlides: false,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 })
