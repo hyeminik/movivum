f0feea938a1e279ff74e62bbd7eb2d124921b435
Seongmin Park <wisesunday96@gmail.com>
2023-02-21 18:02:04 +0900

    탭메뉴 토글

--- js/mypage_FAQ.js
+++ js/mypage_FAQ.js
@@ -1,11 +1,16 @@
 $(function(){
-    $('.answerbtn').on('click',function(){
-            $('.faq_answer_list_answer').toggleClass('seebtn');
-    });
+    $(".blocks_item_link").click(function(){
+        $(".blocks_item_link").removeClass("selected");
+        $(this).addClass("selected");
+        $(".faq_answer_blocks").hide();
+        $($(this).attr("href")).show();
+        console.log("hello");
+    })
 })
 
 $(function(){
-    $('.answerbtn').on('click',function(){
-            $(this).toggleClass('seebtn_angle');
-    })
-})
+    $(".faq_answer_list>li").click(function(){
+        const test = $(this).next("div").slideToggle(200);
+        $(this).parent("ul").siblings("ul").children("div").slideUp(200);
+    });
+});
