9b48c21e27bfc13d6b324e85ede599826bef64fb
Seongmin Park <wisesunday96@gmail.com>
2023-02-24 00:17:30 +0900

    api fetch (임의)

--- js/person.js
+++ js/person.js
@@ -1,18 +1,32 @@
-$(function(){
-    console.log('hi');
-    $.ajax({
-        url: "https://api.themoviedb.org/3/movie/popular?api_key=ce8ce557f85eb5b4175ee66f08002107&language=ko-KR&page=5", //-> 예시... 인물이랑 관련작품
-        data: {"key":"value"},
-        type: "POST",
-        success: function(json){
-            console.log(json);
-            
-            let movie_list=json.results;
-            for(let i=o; i<movie_list.length; i++){
-                let card=`<div class=card>
-                `
-            }
+//api
+$(document).ready(function(){
+    fetch('https://api.themoviedb.org/3/person/12345?api_key=ce8ce557f85eb5b4175ee66f08002107&language=ko-KR')
+    .then((response) => response.json())
+    .then((data) => {
+        const name = document.createElement("span");
+        name.className = 'actor_name';
+        name.textContent = data.name;
 
-        }
-    })
-})
+        const job = document.createElement("span");
+        job.className = 'job';
+        job.textContent = data.known_for_department;
+
+        const date = document.createElement("span");
+        date.className = 'date';
+        date.textContent = data.birthday;
+
+        const poster =document.createElement("img");
+        poster.src=`https://image.tmdb.org/t/p/w500/${data.profile_path}`;
+
+        // for( let i=0; i<data.genres.length; i++ ){
+        //     sss += data.genres[i].name+"             ";
+        // console.log(sss);
+        // }
+        document.querySelector(".poster").appendChild(poster);
+        document.querySelector(".name").appendChild(name);
+        document.querySelector(".birth").appendChild(date);
+        document.querySelector(".job").appendChild(job);
+        
+            })
+        })
+    
